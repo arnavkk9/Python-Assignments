b5cac6082c0b58c7185a7df3a46fe22ad3983935
Arnav Khapekar <khapekararnav4@gmail.com>
2026-03-21 23:38:20 +0530

    Refactor input prompts and clean up print statements

--- lab assignment 10(1).py
+++ lab assignment 10(1).py
@@ -1,31 +1,20 @@
 import pandas as pd
 
-# Load CSV file
 df = pd.read_csv("books.csv")
 
-# a) Print complete report
-print("\n--- Complete Book Report ---")
 print(df)
 
-# b) Books of a given author
-author_name = input("\nEnter author name: ")
+author_name = input()
 print(df[df['author'] == author_name])
 
-# c) Books of a given publisher
-publisher_name = input("\nEnter publisher name: ")
+publisher_name = input()
 print(df[df['publisher'] == publisher_name])
 
-# d) Cheapest and costliest book
 cheapest = df.loc[df['price'].idxmin()]
 costliest = df.loc[df['price'].idxmax()]
 
-print("\nCheapest Book:")
 print(cheapest['title'])
-
-print("\nCostliest Book:")
 print(costliest['title'])
 
-# e) Sort by year of publication
 sorted_df = df.sort_values(by='year')
-print("\n--- Books Sorted by Year ---")
 print(sorted_df)
